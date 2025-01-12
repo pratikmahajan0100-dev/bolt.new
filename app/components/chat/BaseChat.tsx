142a43f030fd957533fcbcb32fd98ae7dbde7a9c
Dustin Loring <dustinwloring1988@gmail.com>
2025-01-11 19:40:37 -0500

    fix: various fixes
    fixes various errors and warning

--- app/components/chat/BaseChat.tsx
+++ app/components/chat/BaseChat.tsx
@@ -72,7 +72,7 @@ export const BaseChat = React.forwardRef<HTMLDivElement, BaseChatProps>(
         <div ref={scrollRef} className="flex overflow-y-auto w-full h-full">
           <div className={classNames(styles.Chat, 'flex flex-col flex-grow min-w-[var(--chat-min-width)] h-full')}>
             {!chatStarted && (
-              <div id="intro" className="mt-[26vh] max-w-chat mx-auto">
+              <div id="intro" className="mt-[20vh] max-w-chat mx-auto">
                 <h1 className="text-5xl text-center font-bold text-bolt-elements-textPrimary mb-2">
                   Where ideas begin
                 </h1>
